c12d12bb6ce0a069b33b5fdf98971164ac229eed
alishasalarda <salardaalishanicole6@gmail.com>
2023-05-27 07:46:08 +0800

    LAB2_FINAL

---
+++ app/Models/User.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\User;
+
+use Illuminate\Database\Eloquent\Model;
+
+
+class User extends Model {
+
+    public $timestamps = false;
+
+    protected $table = 'tbluser';
+    protected $primaryKey = 'userid';
+
+    protected $fillable = [
+        'username', 'password', 'gender', 'jobid'
+    ];
+
+    protected $hidden =[
+        "password"
+    ];
+}
